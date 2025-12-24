63dc743b5e4aa08ffc249648de14ba67060d4e9c
TheByronHimes <TheByronHimes@gmail.com>
2025-12-24 12:22:00 +0000

    Replace direct obj storage use in S3Cleaner with S3Client

--- src/dhfs/core/cleaner.py
+++ src/dhfs/core/cleaner.py
@@ -17,17 +17,17 @@
 
 import logging
 
-from ghga_service_commons.utils.multinode_storage import S3ObjectStorages
-
 from dhfs.config import Config
 from dhfs.ports.outbound.central import CentralClientPort
 from dhfs.ports.outbound.cleaner import S3CleanerPort
+from dhfs.ports.outbound.s3 import S3ClientPort
 
 log = logging.getLogger(__name__)
 
 __all__ = ["S3Cleaner"]
 
 
+# TODO: Consolidate this class with the interrogator class
 class S3Cleaner(S3CleanerPort):
     """Performs cleanup for the interrogation bucket"""
 
@@ -36,41 +36,18 @@ class S3Cleaner(S3CleanerPort):
         *,
         config: Config,
         central_client: CentralClientPort,
-        object_storages: S3ObjectStorages,
+        s3_client: S3ClientPort,
     ):
         self._client = central_client
-        self._object_storages = object_storages
+        self._s3_client = s3_client
         self._interrogation_storage_alias = config.interrogation_storage_alias
-
-    class S3CleanupError(RuntimeError):
-        """Raised when there's a problem deleting an object from S3 during cleanup"""
-
-        def __init__(self, *, bucket_id: str, object_id: str):
-            msg = f"Failed to delete object {object_id} from the {bucket_id} bucket."
-            super().__init__(msg)
 
     async def scan_and_clean(self):
         """Get a list of all objects in the 'interrogation' bucket, then query the
         GHGA Central API and delete the objects which that API says may be deleted.
         """
         # TODO: Incomplete MPU cleanup - hexkit currently lacks a 'list ongoing MPUs' method
-        try:
-            bucket_id, object_storage = self._object_storages.for_alias(
-                self._interrogation_storage_alias
-            )
-        except KeyError as exc:
-            storage_alias_not_configured = self.StorageAliasNotConfiguredError(
-                alias=self._interrogation_storage_alias
-            )
-            log.critical(storage_alias_not_configured)
-            raise storage_alias_not_configured from exc
-
-        object_ids = await object_storage.list_all_object_ids(bucket_id)
-        log.info(
-            "Retrieved list of %i object IDs from bucket ID '%s'",
-            len(object_ids),
-            bucket_id,
-        )
+        object_ids = await self._s3_client.list_files_in_interrogation_bucket()
 
         # No need to convert file IDs to UUID here because they are serialized to string
         #  in the outbound request, and S3 expects strings. In short, we don't need the
@@ -78,14 +55,4 @@ class S3Cleaner(S3CleanerPort):
         removable_objects = await self._client.get_removable_files(file_ids=object_ids)
 
         for object_id in removable_objects:
-            try:
-                await object_storage.delete_object(
-                    bucket_id=bucket_id, object_id=str(object_id)
-                )
-                log.info(
-                    "Successfully removed %s from bucket ID %s", object_id, bucket_id
-                )
-            except Exception as err:
-                error = self.S3CleanupError(bucket_id=bucket_id, object_id=object_id)
-                log.error(error, exc_info=True)
-                raise error from err
+            await self._s3_client.remove_file(object_id=object_id)
--- src/dhfs/ports/outbound/s3.py
+++ src/dhfs/ports/outbound/s3.py
@@ -26,13 +26,57 @@ class S3ClientPort(ABC):
     class S3Error(RuntimeError):
         """Raised when there's a problem with an operation in S3"""
 
+    class BucketNotFoundError(S3Error):
+        """Raised when a given bucket does not exist in S3"""
+
+    class ObjectNotFoundError(S3Error):
+        """Raised when a given object does not exist in S3"""
+
+    class DownloadError(S3Error):
+        """Raised when there's a problem downloading a file from the inbox."""
+
+    class UploadInitError(S3Error):
+        """Raised when there's a problem initiating an upload to the interrogation bucket."""
+
+    class UploadError(S3Error):
+        """Raised when there's a problem uploading a file part to the interrogation bucket."""
+
+    class BadPartMD5Error(UploadError):
+        """Raised when the MD5 for a file part doesn't match the expected value"""
+
+        def __init__(self, *, part_no: int, object_id: str):
+            msg = (
+                f"Failed to upload part {part_no} for file {object_id} because the"
+                + " MD5 hash didn't match the expected value."
+            )
+            super().__init__(msg)
+
+    class UploadCompletionError(S3Error):
+        """Raised when there's a problem completing an upload"""
+
+    class S3CleanupError(RuntimeError):
+        """Raised when there's a problem deleting an object from S3"""
+
+        def __init__(self, *, bucket_id: str, object_id: str):
+            msg = f"Failed to delete object {object_id} from the {bucket_id} bucket."
+            super().__init__(msg)
+
+    class InvalidURLError(S3Error):
+        """Raised when an upload or download URL is rejected with a 403 error"""
+
     @abstractmethod
     async def get_is_file_in_inbox(self, *, file_id: UUID4) -> bool:
         """Return a bool indicating whether the object exists in the inbox"""
 
     @abstractmethod
-    async def fetch_file_part(self, *, object_id: str, start: int, stop: int) -> bytes:
-        """Download a single file part"""
+    async def list_files_in_interrogation_bucket(self) -> list[str]:
+        """Returns a list of object IDs from the interrogation bucket"""
+
+    @abstractmethod
+    async def fetch_file_content_range(
+        self, *, object_id: str, start: int, stop: int
+    ) -> bytes:
+        """Download a single file part for the bytes in range `start` - `stop` (inclusive)."""
         ...
 
     @abstractmethod
@@ -54,7 +98,9 @@ class S3ClientPort(ABC):
         ...
 
     @abstractmethod
-    async def complete_upload(self, *, upload_id: str, object_id: str) -> str:
+    async def complete_upload(
+        self, *, upload_id: str, object_id: str, part_count: int
+    ) -> str:
         """Complete a multipart upload for an object in the interrogation bucket"""
         ...
 
