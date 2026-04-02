e232f0cdeacbebb9a9bed155dcd8476d0b09a414
TheByronHimes <TheByronHimes@gmail.com>
2026-04-02 12:47:20 +0000

    More log less return

--- src/dhfs/config.py
+++ src/dhfs/config.py
@@ -80,9 +80,11 @@ class Config(
     def enforce_client_reraise_from_retry_error_false(cls, value: bool) -> bool:
         """Enforce the False setting for client_reraise_from_retry_error"""
         if value:
-            log.info("Forcing config value `client_reraise_from_retry_error` to False.")
-            return False
-        return value
+            log.info(
+                "Forcing config value `client_reraise_from_retry_error` to False, as"
+                + " that is the only supported value for this application."
+            )
+        return False
 
 
 CONFIG = Config()  # type: ignore
